935a24afb1632f154e8d432a282d0691924f6b6b
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2026-03-23 10:33:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Githubsoniaryan2/SwastiMusicBot",
+    "https://github.com/Githubsoniaryan2/SmartMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
